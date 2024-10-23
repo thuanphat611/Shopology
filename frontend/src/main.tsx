175e989e57aa7d32c37635d531f7a76e7f1a8881
thuanphat611 <phat06112003@gmail.com>
2024-10-23 15:22:25 +0700

    refactor: set up Taillwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "./index.css";
 import AppRouter from "./routes";
 
 createRoot(document.getElementById("root")!).render(
