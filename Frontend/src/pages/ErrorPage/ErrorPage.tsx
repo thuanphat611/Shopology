35ed0eda70e4c0caab3867b6712dcfc7415159e6
thuanphat611 <phat06112003@gmail.com>
2024-11-02 16:47:31 +0700

    feat: add error page

---
+++ Frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import { Breadcrumb, Button } from "antd";
+
+import { Header, Footer } from "@/layouts/DefaultLayout";
+
+export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col min-h-[100vh]">
+      <Header />
+      <div className="w-full max-w-screen-xl px-10 mb-20 grow lg:px-0 lg:mx-auto">
+        <div className="mt-[80px] mb-[140px]">
+          <Breadcrumb
+            items={[
+              {
+                title: <a href="/home">Home</a>,
+              },
+              {
+                title: "404 Error",
+              },
+            ]}
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="text-center text-[110px] font-semibold leading-[115px] mb-10">
+            404 NOT FOUND
+          </h1>
+          <p className="text-[16px] mb-24">
+            Your visited page not found. You may go home page.
+          </p>
+          <Button
+            type="primary"
+            style={{
+              backgroundColor: "#DB4444",
+              fontSize: "16px",
+              lineHeight: "24px",
+              borderRadius: "4px",
+              padding: "16px 48px",
+            }}
+            size="large"
+            onClick={() => navigate("/home")}
+          >
+            Back to home page
+          </Button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
--- Frontend/src/routes/config.tsx
+++ Frontend/src/routes/config.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { DefaultLayout } from "@/layouts";
-import { HomePage, LoginPage } from "@/pages";
+import { HomePage, LoginPage, ErrorPage } from "@/pages";
 
 export default function AppRouter() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <DefaultLayout />,
+      errorElement: <ErrorPage />,
       children: [
         {
           path: "/home",
