9f7d1d3fe2366d3bef9400111585a79801ef9c06
thuanphat611 <phat06112003@gmail.com>
2024-11-02 16:39:42 +0700

    style: config theme

--- Frontend/src/themes/index.ts
+++ Frontend/src/themes/index.ts
@@ -1,7 +1,15 @@
 export const defaultTheme = {
+  token: {
+    fontSize: "16px",
+    lineHeight: "1.5",
+  },
   components: {
     Input: {
       activeBorderColor: "#DB4444",
+    },
+    Breadcrumb: {
+      lastItemColor: "#000000",
+      separatorMargin: "12px",
     },
   },
 };
