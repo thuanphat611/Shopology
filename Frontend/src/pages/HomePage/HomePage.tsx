6bc48002fde683971a3882ef54771bee1a71957b
thuanphat611 <phat06112003@gmail.com>
2024-11-28 18:38:02 +0700

    Feat: Finish category section

--- Frontend/src/pages/HomePage/HomePage.tsx
+++ Frontend/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router-dom";
+
+import { SaleBanner } from "./components";
+
 export default function HomePage() {
-  return <>Home page</>;
+  const list = [
+    "Woman's Fashion",
+    "Men’s Fashion",
+    "Electronics",
+    "Home & Lifestyle",
+    "Medicine",
+    "Sports & Outdoor",
+    "Baby’s & Toys",
+    "Groceries & Pets",
+    "Health & Beauty",
+  ];
+
+  return (
+    <>
+      <div className="flex lg:gap-[45px]  mb-20 md:mb-32 lg:mb-[140px]">
+        <ul className="basis-1/5 flex-col pt-10 pr-4 gap-4 hidden lg:flex items-start border-r border-[#ccc]">
+          {list.map((item, index) => (
+            <li
+              key={index}
+              className="text-[16px] leading-normal hover:text-button-hover-red"
+            >
+              <Link to="">{item}</Link>
+            </li>
+          ))}
+        </ul>
+        <SaleBanner />
+      </div>
+    </>
+  );
 }
